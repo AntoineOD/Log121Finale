e9b1d674bdb5d8775411fb715b73b4e595bac963
AntoineOD <antoine141@outlook.com>
2024-04-06 12:49:47 -0400

    FXML fait

--- Laboratoire5/src/main/java/org/example/laboratoire5/Controleur.java
+++ Laboratoire5/src/main/java/org/example/laboratoire5/Controleur.java
@@ -1,5 +1,20 @@
 package org.example.laboratoire5;
 
-public class Controleur {
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
 
+public class Controleur {
+    @FXML
+    ImageView iv1;
+    @FXML
+    ImageView iv2;
+    @FXML
+    ImageView ivMini;
+    Image image = new Image(getClass().getResourceAsStream("/Images/beautiful-fantasy-landscape-desktop-wallpaper-preview.png"));
+    public void initialize() {
+        iv1.setImage(image);
+        iv2.setImage(image);
+        ivMini.setImage(image);
+    }
 }
--- Laboratoire5/src/main/java/org/example/laboratoire5/VuePrincipale.java
+++ Laboratoire5/src/main/java/org/example/laboratoire5/VuePrincipale.java
@@ -24,43 +24,6 @@ public class VuePrincipale extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        GridPane grid = new GridPane();
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(10, 10, 10, 10));
 
-        Button btnSauvegarder = new Button("Sauvegarder");
-        grid.add(btnSauvegarder, 0, 0);
-
-        Button btnQuitter = new Button("Quitter");
-        grid.add(btnQuitter, 1, 0);
-
-        Image image = new Image("file:beautiful-fantasy-landscape-desktop-wallpaper-preview.png");
-        ImageView iv = new ImageView(image);
-        iv.setPreserveRatio(true);
-        iv.setFitWidth(400);
-        grid.add(iv, 2,5);
-
-        Button btnUp = new Button("^");
-        grid.add(btnUp, 4, 10);
-
-        Button btnDown = new Button("⌄");
-        grid.add(btnDown, 4, 12);
-
-        Button btnGauche = new Button("<");
-        grid.add(btnGauche, 3, 11);
-
-        Button btnDroite = new Button(">");
-        grid.add(btnDroite, 5, 11);
-
-        Button btnZoom = new Button("+");
-        grid.add(btnZoom, 7, 10);
-
-        Button btnDeZoom = new Button("-");
-        grid.add(btnDeZoom, 7, 12);
-
-        Scene scene = new Scene(grid, 750, 500);
-        primaryStage.setScene(scene);
-        primaryStage.show();
     }
 }
