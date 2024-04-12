f5e85074c91d6aecc035978c6fe2882597386ed2
AntoineOD <antoine141@outlook.com>
2024-04-12 16:31:25 -0400

    Modification de vueImg1 en vueImg sans peter le code

--- Laboratoire5/src/main/java/org/example/laboratoire5/VueImg.java
+++ Laboratoire5/src/main/java/org/example/laboratoire5/VueImg.java
@@ -3,20 +3,24 @@ package org.example.laboratoire5;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 
-public class VueImg implements Observer{
-    Controleur controleur;
-    @FXML ImageView iv1;
-    public VueImg(ImageView imageView){
-        iv1=imageView;
+import java.io.Serializable;
+
+public class VueImg implements Observer, Serializable {
+    transient Controleur controleur;
+    @FXML transient ImageView iv1;
+
+    public VueImg(ImageView imageView) {
+        this.iv1 = imageView;
     }
 
     @Override
     public void update(Subject s) {
-        iv1.setImage(s.getImage());
-        iv1.setScaleX( s.getCurrentScale());
-        iv1.setScaleY(s.getCurrentScale());
-        iv1.setTranslateX(s.getPositionX());
-        iv1.setTranslateY(s.getPositionY());
+        if (iv1 != null) {
+            iv1.setImage(s.getImage());
+            iv1.setScaleX(s.getCurrentScale());
+            iv1.setScaleY(s.getCurrentScale());
+            iv1.setTranslateX(s.getPositionX());
+            iv1.setTranslateY(s.getPositionY());
+        }
     }
-
 }
